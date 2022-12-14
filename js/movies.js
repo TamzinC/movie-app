712a6d97fe64d05e0ad6662a4aa52981b8651db9
TamzinC <115940593+TamzinC@users.noreply.github.com>
2022-12-14 20:49:58 +0000

    initial

---
+++ js/movies.js
@@ -0,0 +1,38 @@
+var movieData = [
+    {
+        title: 'Jurassic Park',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        imdb_link: 'https://www.imdb.com/title/tt13443470/?ref_=hm_fanfav_tt_i_1_pd_fp1',
+        movie_image: 'https://api.lorem.space/image/movie?w=150&h=220'
+    },
+    {
+        title: 'Matrix',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        imdb_link: 'https://www.imdb.com/title/tt13443470/?ref_=hm_fanfav_tt_i_1_pd_fp1',
+        movie_image: 'https://api.lorem.space/image/movie?w=150&h=220'
+    },
+    {
+        title: 'Avatar',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        imdb_link: 'https://www.imdb.com/title/tt13443470/?ref_=hm_fanfav_tt_i_1_pd_fp1',
+        movie_image: 'https://api.lorem.space/image/movie?w=150&h=220'
+    },
+    {
+        title: 'Godfather',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        imdb_link: 'https://www.imdb.com/title/tt13443470/?ref_=hm_fanfav_tt_i_1_pd_fp1',
+        movie_image: 'https://api.lorem.space/image/movie?w=150&h=220'
+    },
+    {
+        title: 'Legally Blonde',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        imdb_link: 'https://www.imdb.com/title/tt13443470/?ref_=hm_fanfav_tt_i_1_pd_fp1',
+        movie_image: 'https://api.lorem.space/image/movie?w=150&h=220'
+    },
+    {
+        title: 'Back to the Future',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        imdb_link: 'https://www.imdb.com/title/tt13443470/?ref_=hm_fanfav_tt_i_1_pd_fp1',
+        movie_image: 'https://api.lorem.space/image/movie?w=150&h=220'
+    }
+];
